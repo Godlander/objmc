9dea97c9ac2a7f11f752023216eb9557f598f724
sheumais <88064574+sheumais@users.noreply.github.com>
2024-04-26 01:15:00 +1000

    use default fog function everywhere

--- objmc/assets/minecraft/shaders/include/fog.glsl
+++ objmc/assets/minecraft/shaders/include/fog.glsl
@@ -17,16 +17,6 @@ float linear_fog_fade(float vertexDistance, float fogStart, float fogEnd) {
     return smoothstep(fogEnd, fogStart, vertexDistance);
 }
 
-float fog_distance_custom(mat4 modelViewMat, vec3 pos, int shape) {
-    if (shape == 0) {
-        return length((modelViewMat * vec4(pos, 1.0)).xyz);
-    } else {
-        float distXZ = length((modelViewMat * vec4(pos.x, 0.0, pos.z, 1.0)).xyz);
-        float distY = length((modelViewMat * vec4(0.0, pos.y, 0.0, 1.0)).xyz);
-        return max(distXZ, distY);
-    }
-}
-
 float fog_distance(vec3 pos, int shape) {
     if (shape == 0) {
         return length(pos);
